e79eb5eca0c616f9e81ef6d0312de0c396508829
Maiilaa <zirarimaila8@gmail.com>
2024-08-30 12:00:26 +0200

    ajout images

--- index.php
+++ index.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Memory Game</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="Assets/css/index.css?t=<?=time()?>">
 </head>
 <body>
     <main>
-        <h1>Memory Game</h1>
+        <?php include '_header.php'; ?>
+        <form action="index.php" method="post">
+            <label for="Nom">Entrez votre nom</label>
+            <input type="Nom" name="Nom">
+            <button type="submit">Jouer</button>
+
     </main>
 </body>
 </html>
